21915efcf9f0b82c6198d2afed855dab50325ec0
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-10-27 07:21:48 +0000

    Update desBio.js

--- src/utils/desBio.js
+++ src/utils/desBio.js
@@ -1,6 +1,5 @@
 const designBio = {
-    description: `My name is Nneka, (feel free to call me Nika, as many people struggle to pronounce the name). I'm an illustrator/ visual designer currently residing in The Hague.
-
+    description: `My name is Karan Raj, (feel free to call me Karan, as many people struggle to pronounce the name). I'm fontUI/app developper over Telegram and Whatsapp currently residing in The India an Student.
     Coming from a creative background taught me to carefully consider my audience and what experience I intend for them to have through my work. The projects I'm most passionate to work on are those where I get the opportunity to tell a story through UI effects, animations, and creating a dynamic user experience.
     
     Here are a few technologies I've been working with recently:`,
