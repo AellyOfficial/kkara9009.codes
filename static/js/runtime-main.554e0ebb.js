deb030eb6eec1745fbd2a1d5f4c4aa4cf1fb35d8
Ananya <itsme.ananya47@gmail.com>
2022-12-04 11:12:06 +0000

    💗

--- static/js/runtime-main.554e0ebb.js
+++ static/js/runtime-main.554e0ebb.js
@@ -60,8 +60,8 @@
         (l.o = function (e, r) {
             return Object.prototype.hasOwnProperty.call(e, r);
         }),
-        (l.p = "/kkara9009.tk/");
-    var f = (this.webpackJsonpkkara9009.tk = this.webpackJsonpkkara9009.tk || []),
+        (l.p = "/kkara9009/");
+    var f = (this.webpackJsonpkkara9009 = this.webpackJsonpkkara9009 || []),
         i = f.push.bind(f);
     (f.push = r), (f = f.slice());
     for (var p = 0; p < f.length; p++) r(f[p]);
